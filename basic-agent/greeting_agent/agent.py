0de1b2e8b730e9a1a9fadc2db487c233a1567a5d
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-07-19 23:29:34 +0100

    finish the basic agent and the agent tools

--- basic-agent/greeting_agent/agent.py
+++ basic-agent/greeting_agent/agent.py
@@ -1,11 +1,24 @@
 from google.adk.agents import Agent
+from datetime import datetime
+from google.adk.tools import google_search
+
+def current_time() -> dict :
+ """
+ Get the current time in the format YYYY-MM-DD HH-MM-SS
+ """
+
+ return {
+  "current_time" : datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+ }
+
 
 root_agent = Agent(
  name="greeting_agent",
  model="gemini-2.0-flash",
  description="this is the greeting agent ",
  instruction="""
- your are a helpful ai agent that greets the user.
- Ask for the user's name then greet them by name .
- """
+ your are a helpful assistant  that use the following tools 
+ - google_search
+ """ ,
+ tools=[google_search]
 )
