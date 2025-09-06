a133854d4b75ab9407b30c6e21b13935215f2fb1
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-09-06 23:51:02 +0100

    update tool context

--- persistant_storage/memory_agent/agent.py
+++ persistant_storage/memory_agent/agent.py
@@ -1,1 +1,8 @@
+from google.adk.tools.tool_context import ToolContext
 
+def add_reminder(reminder:str , tool_context: ToolContext) -> dict:
+ """Add a new reminder to the user's reminder list.
+ Args:
+  reminder: The reminder text to add 
+  tool_context: Context for accessing
+ """
